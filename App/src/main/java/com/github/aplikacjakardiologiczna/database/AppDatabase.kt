557d1d72181595521744195ea0890c1766ac76f4
karolina9755 <65297866+karolina9755@users.noreply.github.com>
2020-06-13 05:10:29 +0200

    #20: Category field added to Task. Category enum added.

--- App/src/main/java/com/github/aplikacjakardiologiczna/database/AppDatabase.kt
+++ App/src/main/java/com/github/aplikacjakardiologiczna/database/AppDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 
 @Database(entities = arrayOf(Task::class), version = 1)
+@TypeConverters(Converter::class)
 
 abstract class AppDatabase : RoomDatabase() {
 
---
+++ App/src/main/java/com/github/aplikacjakardiologiczna/database/Category.kt
@@ -0,0 +1,10 @@
+package com.github.aplikacjakardiologiczna.database
+
+enum class Category(val value: String) {
+
+    EXCERCISE("ćwiczenia fizyczne"),
+    FOOD("zdrowe odżywianie"),
+    MEDICINE("przyjmowanie leków"),
+    PULSE("pomiar ciśnienia")
+
+}
