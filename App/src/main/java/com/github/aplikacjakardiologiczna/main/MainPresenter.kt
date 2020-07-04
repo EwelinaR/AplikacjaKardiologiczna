9ac1f1ec443273e217c205c6c46cd8eda4c972ce
dorotatomczak <dorotatomczak@carfax.com>
2020-07-04 13:55:18 +0200

    #13: Refactor

--- App/src/main/java/com/github/aplikacjakardiologiczna/main/MainPresenter.kt
+++ App/src/main/java/com/github/aplikacjakardiologiczna/main/MainPresenter.kt
@@ -1,18 +1,8 @@
 package com.github.aplikacjakardiologiczna.main
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlin.coroutines.CoroutineContext
-
-class MainPresenter(view: MainContract.View,
-                    private val uiContext: CoroutineContext = Dispatchers.Main) : MainContract.Presenter, CoroutineScope {
+class MainPresenter(view: MainContract.View) : MainContract.Presenter {
 
     private var view: MainContract.View? = view
-    private var job: Job = Job()
-
-    override val coroutineContext: CoroutineContext
-        get() = uiContext + job
 
     override fun onViewCreated() {
         view?.showHeartView()
